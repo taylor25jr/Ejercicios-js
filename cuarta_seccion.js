892e02a1f1316571ab0db210fb34c5e9a3097e7f
Taylor jr <luisdavidferrerconde@gmail.com>
2023-05-24 23:16:25 -0500

    resolving 4th excercises section

---
+++ cuarta_seccion.js
@@ -0,0 +1,29 @@
+/*
+12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, 
+pe. miFuncion(7) devolverá true.
+13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
+14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
+*/
+
+//SOLUCION 13
+const verify = (num = undefined) => 
+(num === undefined) ? console.log('ingresa un numero') 
+:(typeof num !== 'number') ? console.log('solo numeros')
+: (num % 2 === 0) ? console.log('es par')
+:(num % 2 != 0) ? console.log('es impar')
+: console.log();
+    
+verify()
+
+//SOLUCION 14
+
+const grados = (grade, type = '') => {
+
+    if(!grade) console.log('ingrese un valor a convertir');
+    if(!type) console.log('ingrese una unida a convertir');
+    if(typeof grade != 'number') console.log('ingrese un numero');
+    if(typeof type != 'string') console.log('indique la unidad de medida en string');
+
+}
+
+grados();
