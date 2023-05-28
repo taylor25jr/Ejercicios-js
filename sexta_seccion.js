2dce9eb83a33bd42da93f11d8e32cfa9e5ca39f7
Taylor jr <luisdavidferrerconde@gmail.com>
2023-05-28 15:17:39 -0500

    6th section finished

--- sexta_seccion.js
+++ sexta_seccion.js
@@ -31,3 +31,34 @@ const contador = (texto) => {
 contador('')
 
 // Cabe resaltr que las validaciones no pueden ser vistas en la consola de quokka sino en la consola del navegador que es la que permite el tipo de console proporcionado.
+
+
+//SOLUCION 19
+
+const validarNombre = (nombre = '') => {
+
+    if (!nombre) console.log('ingresa un nombre');
+    if (typeof nombre !== 'string') console.log('ingresa un string valido');
+
+    let expREg = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜÙŪ\s]+¢/g.test(nombre);
+
+    (nombre) ? console.log('Nombre ingresado correctamente')
+        : console.log('Nombre invalido,vuelva a igresarlo')
+}
+
+validarNombre()
+
+//SOLUCION 20
+
+const validarCorreo = (correo = '') => {
+    if (!correo) console.log('ingresa un correo');
+    if (typeof correo !== 'string') console.log('ingresa un string valido');
+
+    let expREg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g.test(correo);
+
+    (expREg) ? console.log('gracias por ingresar un correo valido')
+        : console.log('vuelve a ingresar un correo valido');
+};
+
+
+validarCorreo('')
